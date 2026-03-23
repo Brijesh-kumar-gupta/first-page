787fd8656feffb9271f78c0b1d1080841418dcf6
Brijesh-kumar-gupta <bk8701154@gmail.com>
2026-03-23 13:41:35 +0530

    Add files via upload

---
+++ HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+       <div className='p-6'>
+        <h3 className='mb-6  text-7xl font-bold'>Prospective <br /><span className='text-gray-500'>customer</span> <br />Segmentation</h3>
+        <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem beatae temporibus fuga ipsam possimus! Quisquam cumque deleniti animi odio ad!</p>
+    </div>
+  )
+}
+
+export default HeroText
